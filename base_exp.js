30739c67851c9273d144b1a7ed951000f16c3657
Garrett Greeley <garrett.greeley@stonybrook.edu>
2021-01-01 18:20:03 -0500

    added encoding two

--- base_exp.js
+++ base_exp.js
@@ -7,8 +7,6 @@ var cond_id = jsPsych.randomization.sampleWithoutReplacement(cond_id_temp, 1);
 var instr1 = {
     type: 'html-keyboard-response',
     stimulus: 'hello 1 hello 1 hello 1',
-    choices: jsPsych.NO_KEYS,
-    trial_duration: 3000,
 };
 
 timeline.push(instr1);
@@ -51,9 +49,26 @@ var encoding_1 = {
         }
     ],
     timeline_variables: study_list_1
-}
+};
 
-timeline.push(encoding_1)
+timeline.push(encoding_1);
+
+var encoding_2 = {
+    timeline: [
+            fixation,
+        {
+            type: 'html-keyboard-response',
+            stimulus: jsPsych.timelineVariable('stim'),
+            choices: [1,2,3,4,5],
+            trial_duration: 3000,
+            prompt: '(very unpleasant) 1 --- 2 --- 3 --- 4 --- 5 (very pleasant)',
+            response_ends_trial: false
+        }
+    ],
+    timeline_variables: study_list_2
+};
+
+timeline.push(encoding_2);
 
 jsPsych.init({
     timeline: timeline
