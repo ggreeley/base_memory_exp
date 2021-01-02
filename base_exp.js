67a6243ead2ea35dccb579805d6fe634bc3a8e5d
Garrett Greeley <garrett.greeley@stonybrook.edu>
2021-01-02 16:36:24 -0500

    custom css, recall collection

--- base_exp.js
+++ base_exp.js
@@ -6,16 +6,16 @@ var cond_id = jsPsych.randomization.sampleWithoutReplacement(cond_id_temp, 1);
 
 var instr1 = {
     type: 'html-keyboard-response',
-    stimulus: 'hello 1 hello 1 hello 1',
+    stimulus: '<h1>Instructions Page 1:</h1>' +
+    'Press "enter" or "return" to continue'
 };
 
 timeline.push(instr1);
 
 var instr2 = {
     type: 'html-keyboard-response',
-    stimulus: 'hello 2 hello 2 hello 2',
-    choices: jsPsych.NO_KEYS,
-    trial_duration: 3000,
+    stimulus: '<h1>Instructions Page 2:</h1>' +
+    'Press "Enter" to Begin'
 }
 
 timeline.push(instr2);
@@ -43,7 +43,7 @@ var encoding_1 = {
             type: 'html-keyboard-response',
             stimulus: jsPsych.timelineVariable('stim'),
             choices: [1,2,3,4,5],
-            trial_duration: 3000,
+            trial_duration: 1000,
             prompt: '(very unpleasant) 1 --- 2 --- 3 --- 4 --- 5 (very pleasant)',
             response_ends_trial: false
         }
@@ -53,6 +53,20 @@ var encoding_1 = {
 
 timeline.push(encoding_1);
 
+var recall_1 = {
+    type: 'survey-html-form',
+    preamble: '<p> Please type all the words you can remember, in any order that you prefer. </p>' +
+    '<p> After entering a word, use a comma and press enter/return to begin entering the next word. </p>',
+    // relies on custom css / textarea html - very handy for recall 
+    html: 
+    '<div class="inline-div"><textarea name="recall1box1" type="text" id="test-resp-box" class="inline-textarea"></textarea></div>' +
+    '<div class="inline-div"><textarea name="recall1box2" type="text" id="test-resp-box" class="inline-textarea"></textarea></div>',
+    autofocus: 'test-resp-box',
+    trial_duration: 10000
+}; 
+
+timeline.push(recall_1);
+
 var encoding_2 = {
     timeline: [
             fixation,
@@ -60,7 +74,7 @@ var encoding_2 = {
             type: 'html-keyboard-response',
             stimulus: jsPsych.timelineVariable('stim'),
             choices: [1,2,3,4,5],
-            trial_duration: 3000,
+            trial_duration: 1000,
             prompt: '(very unpleasant) 1 --- 2 --- 3 --- 4 --- 5 (very pleasant)',
             response_ends_trial: false
         }
@@ -71,5 +85,6 @@ var encoding_2 = {
 timeline.push(encoding_2);
 
 jsPsych.init({
-    timeline: timeline
+    timeline: timeline,
+    show_progress_bar: true
 });
